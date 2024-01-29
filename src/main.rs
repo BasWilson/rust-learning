ea0204885517747d961bcfcb70e60f01cd1cd34a
Bas Wilson <friedcatps@gmail.com>
2024-01-29 10:44:22 +0100

    chapter 5

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    string_on_stack();
+}
+
+fn word_guessing_game() {
     println!("Guess the number!");
 
     loop {
@@ -179,4 +183,212 @@ fn for_rev() {
         println!("{number}!")
     }
     println!("Lift off!!")
+}
+
+fn string_on_stack() {
+    // this string can be on the stack because it's size is known before compiling
+    let mut s = "a string on the stack";
+    print!("The string: {s}");
+
+    s = "a string on the stack. edited";
+    print!("The string edited: {s}")
+
+    // as you can see you can still change the string, but you are not changing the string literal itself.
+    // instead youre change which string s refers to. look at the example below for a string that you can change
+}
+
+fn string_in_heap() {
+    let mut s = String::from("test");
+    print!("The string: {s}");
+    s.push_str(", another test");
+    print!("The string: {s}");
+}
+
+fn copying_stuff_from_heap() {
+    let s1 = String::from("hello");
+    let s2 = s1;
+
+    // using s1 here is impossible, since it has now been "moved" to s2. It is no longer valid as s1
+    // https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html
+    println!("{}, world!", s2);
+    // this makes sure that when both go out of scope, freeing up the memory is only called once (on s2).
+    // and wont introduce any bugs
+}
+
+fn cloning() {
+    let s1 = String::from("Hello");
+    let s2 = s1.clone();
+    // cloning is an expensive operation.
+    println!("s1: {s1}, s2: {s2}")
+}
+
+fn copying_stuff_on_stack() {
+    let x = 5;
+    let y = x;
+
+    // using y here is POSSIBLE. Unlike the String type above. the size of x is known.
+    // x lives entirely on the stack. which makes copying it very fast.
+    println!("{}, world!", y);
+}
+
+fn copy_and_ownership_with_functions() {
+    // copying and owning works the same when  passing vars to a function as it does with assigning to different vars
+    let s = String::from("hello");
+    takes_ownership(s);
+    // using "s" leter in this scope is not possible, this would error.
+
+    // you can hover make takes_ownership return the value after using it!
+    let s = takes_ownership_and_returns(s);
+    println!("{s}");
+
+    let x = 5;
+    makes_copy(x);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{some_string}")
+}
+
+fn takes_ownership_and_returns(some_string: String) -> String {
+    some_string
+}
+
+fn makes_copy(some_int: i32) {
+    println!("{some_int}")
+}
+
+// https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html#references-and-borrowing
+// here we pass a reference to s1 to the calc_Len function. Unlike a pointer, a reference is guaranteed to point to a valid value of a particular type for the life of that reference.
+fn uses_reference() {
+    let s1 = String::from("Hello");
+    let len = calc_len(&s1);
+    println!("Len of {s1} is: {len}")
+}
+
+fn calc_len(s: &String) -> usize {
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, it is not dropped.
+
+fn try_to_change_borrowed_var(s: &String) {
+    // this does not work, bc this function does not own s
+    // s.push_str(", world");
+}
+
+// to change a borrowed var, it needs to be mutable, and the function's signature also needs to accept a mutable reference
+// you cannot lend out the mut s to two variables if both are used at the same time as in example shown below
+fn change_mutable() {
+    let mut s = String::from("hello");
+    change(&mut s);
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{r1}, {r2}")
+}
+
+fn change(s: &mut String) {
+    s.push_str(" world")
+}
+
+fn multiple_mutable_refs() {
+    let mut s = String::from("hello");
+
+    {
+        let r1 = &mut s;
+    } // r1 goes out of scope here, so we can make a new reference with no problems.
+
+    let r2 = &mut s;
+}
+
+fn mut_and_unmut_mixed() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+                 // let r3 = &mut s; // BIG PROBLEM
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
+
+// this does not work, because s is no longer available after this scope. making it a dangling reference.
+// pointer pointing to nothing :))
+fn dangle() -> &String {
+    // dangle returns a reference to a String
+
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    // iter() returns each element as a collection. enumerate() makes the result of each value in the collection a tuple of (index, item)
+    // if we only used iter(), we would not know the index!
+    for (i, &item) in bytes.iter().enumerate() {
+        // b is a byte literal string
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn test_first_word() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+    s.clear(); // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now totally invalid!
+}
+
+fn string_slices() {
+    let s = String::from("Hello world");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    let hello = &s[..5];
+    let world = &s[6..];
+    let hello_world = &s[..];
+}
+
+fn first_word_rewrite(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn string_literal_slices() {
+    let my_string = String::from("Hello word");
+    let hello = first_word_rewrite(&my_string[0..6]);
+    let hello_world = first_word_rewrite(&my_string[..]);
+
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word_rewrite(&my_string);
+
+    let my_string_lit = "hello world";
+    // `first_word` works on slices of string literals, whether partial or whole
+    let hello = first_word_rewrite(&my_string_lit[0..6]);
+    let hello_world = first_word_rewrite(&my_string_lit[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let hello_world = first_word_rewrite(my_string_lit);
+}
+
+fn other_slices() {
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
 }
